9d9e642ec446d62acb3814e9672cc1fdc6588805
Fifus17 <filipdziurdzia2002@gmail.com>
2022-11-10 14:15:58 +0100

    finished ex9

---
+++ lab3/Zadania/Rozwiązane zadania/zad9/script.js
@@ -0,0 +1,56 @@
+// Getting html elements with DOC API
+let rightButton = document.getElementById("right-arrow");
+let leftButton = document.getElementById("left-arrow");
+let card = document.getElementById("card-container");
+
+// arrays with data to show
+const photos = ["person1.png", "person2.png", "person3.png"];
+const names = ["JAN KOWALSKI", "JANUSZ NOWAK", "GAREK MAJĘCKI"];
+const position = ["Product Manager", "Frontend Developer", "Tech Lead"];
+counter = 0;
+
+// delay function
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
+
+// animation going right
+var animationForward =  () => {
+    card.style.opacity = '0';
+    card.style.animation = 'slideRight 0.5s ease-in-out';
+    counter++;
+    // waiting for the animation to finish
+    delay(1000).then(() => {
+        // changing data
+        card.children[0].src = photos[counter%photos.length];
+        card.children[1].innerText = names[counter%names.length];
+        card.children[2].innerText = position[counter%position.length];
+        // animation to bring the card back
+        card.style.animation = 'slideRightFromLeft 0.5s ease-in-out';
+        card.style.opacity = '1';
+    }
+    );
+}
+
+// animation going left
+var animationBackward =  () => {
+    card.style.opacity = '0';
+    card.style.animation = 'slideLeft 0.5s ease-in-out';
+    counter--;
+    if(counter < 0) counter = 2;
+    // waiting for the animation to finish
+    delay(1000).then(() => {
+        // changing data
+        card.children[0].src = photos[counter%photos.length];
+        card.children[1].innerText = names[counter%names.length];
+        card.children[2].innerText = position[counter%position.length];
+        // animation to bring the card back
+        card.style.animation = 'slideLeftFromRight 0.5s ease-in-out';
+        card.style.opacity = '1';
+    }
+    );
+}
+
+// adding event listeners to the buttons
+rightButton.addEventListener("click", animationForward);
+leftButton.addEventListener("click", animationBackward);
