91f565c38cb482c6399a07975c1a2db5c38bda00
Fifus17 <filipdziurdzia2002@gmail.com>
2022-11-28 21:09:57 +0100

    safe commit

---
+++ MainProject copy/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TripsComponent } from './Components/trips/trips.component';
+import { TripComponent } from './Components/trip/trip.component';
+import { NavigationBarComponent } from './Components/navigation-bar/navigation-bar.component';
+import { ShoppingCartComponent } from './Components/shopping-cart/shopping-cart.component';
+import { ShoppingCartCardComponent } from './Components/shopping-cart-card/shopping-cart-card.component';
+import { AddModalComponent } from './Components/add-modal/add-modal.component';
+import { RemoveTripComponent } from './Components/remove-trip/remove-trip.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TripsComponent,
+    TripComponent,
+    NavigationBarComponent,
+    ShoppingCartComponent,
+    ShoppingCartCardComponent,
+    AddModalComponent,
+    RemoveTripComponent,
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
